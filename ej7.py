0e8dfc38090729cde489bf47a5f2f73da9432c55
juanse132 <juanmanuel1042@hotmail.com>
2021-04-15 00:20:53 -0300

    hecho el ej7

--- ej7.py
+++ ej7.py
@@ -1,9 +1,9 @@
 def bisiesto():
     year=int(input("ingrese un año: "))
-    if (year % 400 != 0 and year % 4 == 0):
+    if (year % 400 == 0 or year % 100 == 0 or year %4 == 0):
         print("es bisiesto el año: ", year)
         return year
-    elif(year % 400 == 0 and year % 100 != 0):
+    elif(year % 4 != 0 and year % 400 != 0):
         print("el año ", year ,"no es bisiesto")
         return year
 print(bisiesto()) 
