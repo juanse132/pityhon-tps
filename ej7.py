c27fd941bc77de1196d760caa8c0c85f3b3dcecd
juanse132 <juanmanuel1042@hotmail.com>
2021-04-15 01:03:15 -0300

    hasta el 7.2

--- ej7.py
+++ ej7.py
@@ -3,7 +3,22 @@ def bisiesto():
     if (year % 400 == 0 or year % 100 == 0 or year %4 == 0):
         print("es bisiesto el año: ", year)
         return year
+        'En el primer if hago si el year es bisiesto'
     elif(year % 4 != 0 and year % 400 != 0):
         print("el año ", year ,"no es bisiesto")
         return year
-print(bisiesto()) 
+        'Y en este if hago si no es bisiesto'
+
+def mes_dias():
+    mes = str(input("Ingrese un mes: "))
+    'Solo se admiten los meses en mayuscula'
+    if(mes == "ENERO" or mes == "MARZO" or  mes == "MAYO" or  mes == "JULIO" or  mes == "AGOSTO" or  mes == "OCTUBRE" or  mes == "DICIEMBRE"):
+        print(mes,"tiene 31 dias")
+        return mes
+    elif(mes == "ABRIL" or  mes == "JUNIO" or mes == "SEPTIEMBRE" or mes == "NOVIEMBRE"):
+        print(mes,"tiene 30 dias")
+        return mes
+    else:
+        print(mes,"tiene 28 dias")
+        return mes
+print(mes_dias())
